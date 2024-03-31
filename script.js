801576cef9896a8643d6896d34a4dbc4e5c0086b
yong woo Yun <yongwooyun@hotmail.com>
2024-03-30 23:22:04 -0700

    added redering data

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-
-  // Display the current date in the header of the page
-  var currentDate = dayjs().format('dddd, MMMM D, YYYY');
-  $("#currentDay").text(currentDate);
-
-$(function () {
+//$(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -25,25 +20,73 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+//});
 
+var currentDate = dayjs().format('dddd, MMMM D, YYYY');
+$("#currentDay").text(currentDate);
+
+//when page is loaded show data from local storage
+rendering()
+
+function rendering() {
+  // Retrieve existing data from local storage
+
+  var ScheduleData = JSON.parse(localStorage.getItem("ScheduleMaker")) || [];
+  
+  // loop and input value to each timeBlockContent
+  var selectedData = null;
+    for (var i = 0; i < ScheduleData.length; i++) {
+  
+            selectedData = ScheduleData[i];
+            var classId = ScheduleData[i].timeContentID;
+            var contents = ScheduleData[i].timeBlockContent;
+            $("#" + classId + " textarea").val(contents);
+          // console.log ("#"+classId+ " textarea")
+          // console.log (classId)
+            //console.log (contents)
+      //  }
+    }
+
+
+}
+
+$(function () {
+  // Add a listener for click events on the save button
   $(".saveBtn").on("click", function() {
-    var idTimeClockBlock = $(this).parent().attr("id");
-    var timeclockContents = $(this).siblings("textarea").val();
-    localStorage.setItem(idTimeClockBlock, timeclockContents);
+
+    var timeContentID = $(this).parent().attr("id");
+    var timeBlockContent = $(this).siblings("textarea").val();
+    
+    // Retrieve existing data from local storage
+    var ScheduleMakerData = JSON.parse(localStorage.getItem("ScheduleMaker")) || [];
+    
+    // Check if an item with the same timeContentID already exists
+    var existingIndex = -1;
+for (var i = 0; i < ScheduleMakerData.length; i++) {
+    if (ScheduleMakerData[i].timeContentID === timeContentID) {
+        existingIndex = i;
+        break;
+    }
+}
+    
+    // If an item with the same timeContentID exists, update it
+    if (existingIndex !== -1) {
+      ScheduleMakerData[existingIndex].timeBlockContent = timeBlockContent;
+    } else {
+        // Otherwise, create a new record
+        ScheduleMakerData.push({ timeContentID: timeContentID, timeBlockContent: timeBlockContent });
+    }
+
+    // Save updated data back to local storage
+    localStorage.setItem("ScheduleMaker", JSON.stringify(ScheduleMakerData));
+
+   // localStorage.removeItem("ScheduleMakerData");
+
+   rendering();
+
 
   });
 
-
-
-
-
-
-
-
-
-
-
-
-
+  
 
 });

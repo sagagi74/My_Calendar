215d821688ab103c3963b9870568b98f8279080c
yong woo Yun <yongwooyun@hotmail.com>
2024-03-30 20:38:57 -0700

    adding local storage save function

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+  // Display the current date in the header of the page
+  var currentDate = dayjs().format('dddd, MMMM D, YYYY');
+  $("#currentDay").text(currentDate);
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -20,4 +25,25 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+
+  $(".saveBtn").on("click", function() {
+    var idTimeClockBlock = $(this).parent().attr("id");
+    var timeclockContents = $(this).siblings("textarea").val();
+    localStorage.setItem(idTimeClockBlock, timeclockContents);
+
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
